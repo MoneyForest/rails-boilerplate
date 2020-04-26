c91bed43418d6808c378214e744ed79ef978aa1a
ownweight <kanamorishuhei@gmail.com>
2020-04-26 22:55:30 +0900

    fix home controller spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
+  describe 'GET #home' do
+    context 'current_user exists' do
+      login_user
+      it 'returns http redirect' do
+        get :home
+        expect(response).to have_http_status(:redirect)
+      end
+    end
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    context 'current_user not exists' do
+      it 'returns http success' do
+        get :home
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
+  describe 'GET #top' do
+    context 'when logged in' do
+      before do
+        allow(controller).to receive(:authenticate_user!).and_return true
+      end
+
+      it 'returns http success' do
+        get :top
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when not logged in' do
+      it 'returns http redirect' do
+        get :top
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+  end
+
+  describe 'GET #mypage' do
+    context 'when logged in' do
+      before do
+        allow(controller).to receive(:authenticate_user!).and_return true
+      end
+
+      it 'returns http success' do
+        allow(controller).to receive(:authenticate_user!).and_return true
+        get :mypage
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when not logged in' do
+      it 'returns http redirect' do
+        get :mypage
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'devise'
+
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -33,6 +35,10 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+
+  # Rspec 4.0.0 以降では不要
+  config.render_views
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -60,4 +66,15 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # support
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.extend LoginSupport, :type => :controller
+
+  # devise
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+
+  # factory_bot
+  config.include FactoryBot::Syntax::Methods
 end
