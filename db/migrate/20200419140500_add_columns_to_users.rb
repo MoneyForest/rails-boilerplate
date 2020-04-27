847d56bd4a7917543e66d583f1396843656ebc1c
ownweight <kanamorishuhei@gmail.com>
2020-04-27 10:47:16 +0900

    fix db schema

---
+++ db/migrate/20200419140500_add_columns_to_users.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class AddColumnsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :withdrawal_at, :datetime
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :display_name, :string, default: 'anonymous'
+    add_column :users, :icon_image_path , :string, limit: 2000
+    add_column :users, :background_image_path, :string, limit: 2000
+    add_column :users, :profile, :text
+    add_column :users, :name, :string
+    add_column :users, :birthday, :datetime
+    add_column :users, :gender, :string
+    add_column :users, :zip_code, :integer
+    add_column :users, :prefecture_id, :integer
+    add_column :users, :living_arrangement, :string
+    add_column :users, :family_structure, :string
+    add_column :users, :job, :string
+    add_column :users, :annual_income, :string
+    add_column :users, :memo, :text
+  end
+end
--- db/migrate/20200419140500_add_twitter_login_columns_to_users.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddTwitterLoginColumnsToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
-    add_column :users, :username, :string, default: 'anonymous'
-  end
-end
