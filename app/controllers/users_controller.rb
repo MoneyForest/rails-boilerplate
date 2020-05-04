0f5601a823a3eb962ef19d614884300ccdcbd3d0
ownweight <kanamorishuhei@gmail.com>
2020-05-04 16:48:24 +0900

    add update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,31 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def update
+    @user = User.find(current_user.id)
+    if @user.update_attributes(update_params)
+      redirect_to root_path, notice: '更新しました'
+    else
+      redirect_back(fallback_location: edit_user_path, flash: {
+                      error_messages: @user.errors.full_messages
+                    })
+    end
+  end
+
   def show; end
 
   def edit; end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:display_name,
+                                 :profile,
+                                 :name,
+                                 :birthday,
+                                 :gender,
+                                 :zip_code,
+                                 :address,
+                                 :job)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,19 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable, :omniauthable,
          omniauth_providers: %i[twitter facebook google]
 
+  validates :display_name, length: { in: 1..15 }
+  validates :name, length: { in: 1..15 }
+
+  validate :birthday_is_past
+
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
 
     unless user
       user ||= User.create(
-        uid:      auth.uid,
+        uid: auth.uid,
         provider: auth.provider,
-        email:    User.dummy_email(auth),
+        email: User.dummy_email(auth),
         password: Devise.friendly_token[0, 20]
       )
     end
@@ -24,6 +29,10 @@ class User < ApplicationRecord
   end
 
   private
+
+  def birthday_is_past
+    errors.add(:birthday, 'は今日以前の日付を選択してください') unless birthday < Date.today
+  end
 
   def self.dummy_email(auth)
     "#{auth.uid}-#{auth.provider}@example.com"
