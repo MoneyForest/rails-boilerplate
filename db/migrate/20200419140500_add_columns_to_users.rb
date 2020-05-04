976926b3568ed52f801220dd1c93b07d24351509
ownweight <kanamorishuhei@gmail.com>
2020-05-05 03:59:56 +0900

    fix db schema

--- db/migrate/20200419140500_add_columns_to_users.rb
+++ db/migrate/20200419140500_add_columns_to_users.rb
@@ -2,7 +2,6 @@
 
 class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :withdrawal_at, :datetime
     add_column :users, :provider, :string
     add_column :users, :uid, :string
     add_column :users, :display_name, :string, default: 'anonymous'
