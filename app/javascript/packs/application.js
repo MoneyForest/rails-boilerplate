0e5c5787b1145cfd38442f2c0401e43cf262ae75
ownweight <kanamorishuhei@gmail.com>
2020-05-04 23:25:58 +0900

    add dropdown menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require('jquery');
+require("jquery");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,3 +16,20 @@ require('jquery');
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 //= require materialize
+
+import 'materialize-css/dist/js/materialize'
+
+$(document).on('turbolinks:load', function () {
+  // initialize matelialize
+  $('.sidenav').sidenav();
+  $('.parallax').parallax();
+  $('.dropdown-trigger').dropdown();
+  $('.modal').modal();
+});
+
+// sidenavだけは、進む/戻るで動かなくなるので都度destroyが必要
+$(document).on("turbolinks:before-cache", function () {
+  $('.sidenav').sidenav('destroy');
+  $('.dropdown-trigger').dropdown('destroy');
+  $('.modal').modal('destroy');
+});
