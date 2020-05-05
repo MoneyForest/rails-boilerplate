e7503b6d792245043e2defb4fb7df69919db7353
ownweight <kanamorishuhei@gmail.com>
2020-05-05 19:41:59 +0900

    fix schema

--- db/migrate/20200419140500_add_columns_to_users.rb
+++ db/migrate/20200419140500_add_columns_to_users.rb
@@ -4,9 +4,9 @@ class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
-    add_column :users, :display_name, :string, default: 'anonymous'
-    add_column :users, :icon_image_path, :string, limit: 2000
-    add_column :users, :background_image_path, :string, limit: 2000
+    add_column :users, :display_name, :string
+    add_column :users, :icon_image, :string
+    add_column :users, :background_image, :string
     add_column :users, :profile, :text
     add_column :users, :name, :string
     add_column :users, :birthday, :datetime
