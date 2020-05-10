ba938c8f47aa304c798b018cf65c90f5e6a42f35
ownweight <kanamorishuhei@gmail.com>
2020-05-10 19:56:23 +0900

    add user model spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,16 @@
 
 FactoryBot.define do
   factory :user do
+    id { 1 }
     email { 'hoge@example.com' }
     password { 'foobar' }
-    password_confirmation { 'foobar' }
+    created_at { Time.now }
+    updated_at { Time.now }
+    display_name { 'hogehoge' }
+    profile { 'fuga' }
+    name { 'John Smith' }
+    birthday { '1990-01-01 00:00:00' }
+    gender { 'male' }
+    zip_code { '1234567' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  context 'display_name' do
+    it 'is invalid more than 15 characters' do
+      user = create(:user)
+      expect(user).to be_valid
+
+      user.display_name = 'morethan15characters'
+      expect(user).to be_invalid
+    end
+  end
+
+  context 'birthday' do
+    it 'is invalid set a past date' do
+      user = create(:user)
+      expect(user).to be_valid
+
+      user.birthday = Time.now + (60 * 60 * 24)
+      expect(user).to be_invalid
+    end
+  end
 end
