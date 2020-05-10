de948ccf7f041eda8ed6d6a97c1b5cf586aba6d2
ownweight <kanamorishuhei@gmail.com>
2020-05-10 19:55:58 +0900

    fix omniauth

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -13,4 +13,40 @@ RSpec.describe Users::OmniauthCallbacksController, type: :controller do
       expect { click_link 'Sign in with Twitter' }.to change(User, :count).by(1)
     end
   end
+
+  describe 'callback facebook' do
+    before do
+      OmniAuth.config.mock_auth[:facebook] = facebook_mock
+      visit new_user_registration_path
+    end
+
+    it 'crate a user' do
+      expect { click_link 'Sign in with Facebook' }.to change(User, :count).by(1)
+    end
+  end
+
+  describe 'callback google' do
+    before do
+      OmniAuth.config.mock_auth[:google] = google_mock
+      visit new_user_registration_path
+    end
+
+    it 'crate a user' do
+      expect { click_link 'Sign in with Google' }.to change(User, :count).by(1)
+    end
+  end
+
+  describe 'user is persisted' do
+    before do
+      OmniAuth.config.mock_auth[:google] = google_mock
+      visit new_user_registration_path
+    end
+
+    it 'not crate a user' do
+      expect { click_link 'Sign in with Google' }.to change(User, :count).by(1)
+      sign_out_user
+      visit new_user_registration_path
+      expect { click_link 'Sign in with Google' }.to change(User, :count).by(0)
+    end
+  end
 end
